33e4744bc1e418c0d26c89f6d7d17f3458f80931
Ryan Tin <ryantin526@gmail.com>
2023-03-13 15:18:07 +0100

    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/index.js
+++ src/index.js
@@ -20,24 +20,18 @@ class Board extends React.Component {
   }
 
   render() {
+      let boardSquares = [];
+      for (let row = 0; row < 3; row++) {
+          let boardRow = [];
+          for (let col = 0; col < 3; col++) {
+              boardRow.push(<span key={(row*3)+col}>{this.renderSquare((row * 3) + col)}</span>);
+          }
+          boardSquares.push(<div className="board-row" key={row}> { boardRow } </div>)
+      }
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+       <div>
+        {boardSquares}
+       </div>
     );
   }
 }
@@ -157,26 +151,6 @@ function calculateWinner(squares) {
 }
 
 function moveCoord(square) {
-    switch(square) {
-        case 0:
-            return [1,1];
-        case 1:
-            return [1,2];
-        case 2:
-            return [1,3];
-        case 3:
-            return [2,1];
-        case 4:
-            return [2,2];
-        case 5:
-            return [2,3];
-        case 6:
-            return [3,1];
-        case 7:
-            return [3,2];
-        case 8:
-            return [3,3];
-        default:
-            break;
-    }
+    let coordinates = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3]];
+    return coordinates[square];
 }
